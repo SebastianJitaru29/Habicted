c2a806c1241e88e7819a67a3a5091274a4c0ac5c
vGerJ02 <gjaimejuanguardia@gmail.com>
2024-04-04 15:37:20 +0200

    feat: add floating bar and icons of navbar

--- app/src/main/java/com/example/habicted_app/routes/HomeDestinations.kt
+++ app/src/main/java/com/example/habicted_app/routes/HomeDestinations.kt
@@ -4,33 +4,56 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import com.example.habicted_app.Greeting
+import com.example.habicted_app.R
 
 interface HomeDestination {
-    val icon: ImageVector
+    val icon: @Composable () -> Unit
     val route: String
     val screen: @Composable () -> Unit
 }
 
 
 object Overview : HomeDestination {
-    override val icon = Icons.Filled.Home
+    override val icon =
+        @Composable {
+            Icon(
+                painter = painterResource(id = R.drawable.outline_checklist_24),
+                contentDescription = "Home"
+            )
+        }
     override val route = "home"
-    override val screen: @Composable () -> Unit = { Greeting(name = "HOME") }
+    override val screen = @Composable { Greeting(name = "HOME") }
 }
 
 object Groups : HomeDestination {
-    override val icon = Icons.Filled.Person
-    override val route = "groups"
-    override val screen: @Composable () -> Unit = { Greeting(name = "Groups") }
+    override val icon =
+        @Composable {
+            Icon(
+                painter = painterResource(id = R.drawable.outline_groups_24),
+                contentDescription = "Groups"
+            )
+        }
+    override val route = "home"
+    override val screen = @Composable { Greeting(name = "Groups") }
 }
 
 object Settings : HomeDestination {
-    override val icon = Icons.Filled.Settings
-    override val route = "settings"
-    override val screen: @Composable () -> Unit = { Greeting(name = "Settings") }
+    override val icon =
+        @Composable {
+            Icon(
+                painter = painterResource(id = R.drawable.outline_settings_24),
+                contentDescription = "Home"
+            )
+        }
+    override val route = "home"
+    override val screen = @Composable { Greeting(name = "Settings") }
 }
 
 // Screens to be displayed in the top RallyTabRow
--- app/src/main/java/com/example/habicted_app/screen/MainScreen.kt
+++ app/src/main/java/com/example/habicted_app/screen/MainScreen.kt
@@ -1,7 +1,12 @@
 package com.example.habicted_app.screen
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -9,31 +14,57 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.habicted_app.routes.HomeDestination
 import com.example.habicted_app.routes.Overview
 import com.example.habicted_app.routes.allItemsNav
+import com.example.habicted_app.ui.theme.HabictedAppTheme
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    var currentScreen: HomeDestination by rememberSaveable { mutableStateOf(Overview) }
+    var currentIndex: Int by rememberSaveable {
+        mutableStateOf(0)
+    }
+
+
     Scaffold(
         bottomBar = {
             NavigationBar {
-                allItemsNav.forEach { item ->
+                allItemsNav.forEachIndexed { index, item ->
                     NavigationBarItem(
-                        selected = currentScreen == item,
-                        onClick = { currentScreen = item },
-                        icon = { Icon(imageVector = item.icon, contentDescription = "") })
+                        selected = currentIndex == index,
+                        onClick = { currentIndex = index },
+                        icon = item.icon
+                    )
                 }
+            }
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { /*TODO*/ }
+            ) {
+                Icon(Icons.Filled.Add, "Floating action button.")
             }
         }
     ) { innerPadding ->
+
         Box(modifier = modifier.padding(innerPadding)) {
-            currentScreen.screen()
+            allItemsNav.get(currentIndex).screen()
         }
+    }
+}
+
+
+@Preview
+@Composable
+fun NavBarPrev() {
+    HabictedAppTheme {
+        MainScreen()
     }
 }
 
