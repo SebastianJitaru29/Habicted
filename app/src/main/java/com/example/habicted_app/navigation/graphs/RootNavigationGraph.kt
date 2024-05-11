be202cefd7aeeedb17b0efc349768d95ac89f0a1
vGerJ02 <gjaimejuanguardia@gmail.com>
2024-05-11 20:29:25 +0200

    feat: add login persistence

--- app/src/main/java/com/example/habicted_app/navigation/graphs/RootNavigationGraph.kt
+++ app/src/main/java/com/example/habicted_app/navigation/graphs/RootNavigationGraph.kt
@@ -7,19 +7,19 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.habicted_app.screen.home.HomeScreen
+import com.google.firebase.auth.FirebaseAuth
 
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun RootNavigationGraph(navController: NavHostController) {
-
+    val user = FirebaseAuth.getInstance().currentUser
     NavHost(
         navController = navController,
         route = Graphs.ROOT,
         startDestination = Graphs.AUTHENTICATION
     ) {
         AuthenticationGraph(navController = navController)
-
         composable(route = Graphs.HOME) {
             HomeScreen()
         }
